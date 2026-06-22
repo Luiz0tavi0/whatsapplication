d895c9fad8d9a51c36b386c1a8666c9b8beae268
Luiz0tavi0 <luiz_loon@yahoo.com.br>
2026-06-21 23:55:56 -0300

    feat: assinatura base webhooks

---
+++ app/api/webhooks.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter
+
+w_router = APIRouter(prefix="webhooks", tags=['webhook'])
+
+
+@w_router.post('/')
+async def zapi_status_message_webhook(payload: dict):
+    ...
+
+@w_router.post('/')
+async def wireweb_status_message_webhook(payload: dict):
+    ...
